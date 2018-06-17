b4e0f11d821e898ff60317fb93f99af79e69f4e6
KIHOSI <amie1234@gmail.com>
2018-06-17 18:00:24 +0800

    can play mp3 in python!

--- SlotMachine.py
+++ SlotMachine.py
@@ -1,22 +1,28 @@
 from Seven_Segement import SevenSegement #import Seven_Segement.py
 import random
+import os
 
 # 設定每一個七節管(Wpi)       
 seg1 = SevenSegement(7,0,2,3,12,13,14)
 seg2 = SevenSegement(21,22,23,24,25,29,28)
 seg3 = SevenSegement(27,26,11,10,6,5,4)
 
+#Game Start
 while True:
-    pinNum = input("請輸入你想顯示的數字(離開請輸入'exit'):")
+    pinNum = input("請按Enter開始遊戲(離開請輸入'exit'):")
     if pinNum == "exit":
         seg1.closeAllLight()
         seg2.closeAllLight()
         seg3.closeAllLight()
         break
-    else:    
-        seg1.call7Segment(random.randint(0,9)) # String to int
-        seg2.call7Segment(random.randint(0,9)) # 隨機產生0~9
-        seg3.call7Segment(random.randint(0,9))
-#seg.call7Segment(0)
-#seg.closeAllLight()
-#seg.openLight(3)
+    else:
+        randNum1 = random.randint(0,9)
+        randNum2 = random.randint(0,9)
+        randNum3 = random.randint(0,9)   
+        seg1.call7Segment(randNum1) # String to int
+        seg2.call7Segment(randNum2) # 隨機產生0~9
+        seg3.call7Segment(randNum3)
+        if(randNum1 == randNum2==randNum3): #如果顯示數字都相同
+            print("bingo!!!")
+            #放勝利音樂
+            os.system('mpg321 /home/pi/SlotMachineFile/I.mp3');
